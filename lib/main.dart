615cdb40383b14c5c9e2defbae4a68ef3a837fdb
Azriel408 <anhldhe182061@fpt.edu.vn>
2026-07-07 01:42:29 +0700

    Cấu hình Firebase: Thêm options và initializeApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:werewolf/screens/main_menu_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const WerewolfApp());
 }
 
